aab8b978f70421cd078c70c0b1d342200d079601
mlietti1 <mcf.lietti@outlook.com>
2022-10-25 16:56:31 +0200

    milestone 1

---
+++ js/script.js
@@ -0,0 +1,38 @@
+const team = [
+  {
+    name: "Wayne Barnett",
+    role: "Founder & CEO",
+    image: "../img/wayne-barnett-founder-ceo.jpg"
+  },
+  {
+    name: "Angela Caroll",
+    role: "Chief Editor",
+    image: "../img/angela-caroll-chief-editor.jpg"
+  },
+  {
+    name: "Walter Gordon",
+    role: "Office Manager",
+    image: "..img/walter-gordon-office-manager.jpg"
+  },
+  {
+    name: "Angela Lopez",
+    role: "Social Media Manager",
+    image: "../img/angela-lopez-social-media-manager.jpg"
+  },
+  {
+    name: "Scott Estrada",
+    role: "Developer",
+    image: "../img/scott-estrada-developer.jpg"
+  },
+  {
+    name: "Barbara Ramos",
+    role: "Graphic Designer",
+    image: "barbara-ramos-graphic-designer.jpg"
+  }
+]
+
+for (let member of team){
+  console.log('name: ' + member.name);
+  console.log('role: ' + member.role);
+  console.log('image tag: ' + member.image);
+}
