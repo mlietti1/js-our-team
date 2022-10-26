2b58a9d142665c7342c81df5c6292f8c0d0a94e1
mlietti1 <mcf.lietti@outlook.com>
2022-10-26 11:17:21 +0200

    correzione

--- js/script.js
+++ js/script.js
@@ -2,54 +2,102 @@ const team = [
   {
     name: "Wayne Barnett",
     role: "Founder & CEO",
-    image: "../img/wayne-barnett-founder-ceo.jpg"
+    image: "wayne-barnett-founder-ceo.jpg"
   },
   {
     name: "Angela Caroll",
     role: "Chief Editor",
-    image: "../img/angela-caroll-chief-editor.jpg"
+    image: "angela-caroll-chief-editor.jpg"
   },
   {
     name: "Walter Gordon",
     role: "Office Manager",
-    image: "../img/walter-gordon-office-manager.jpg"
+    image: "walter-gordon-office-manager.jpg"
   },
   {
     name: "Angela Lopez",
     role: "Social Media Manager",
-    image: "../img/angela-lopez-social-media-manager.jpg"
+    image: "angela-lopez-social-media-manager.jpg"
   },
   {
     name: "Scott Estrada",
     role: "Developer",
-    image: "../img/scott-estrada-developer.jpg"
+    image: "scott-estrada-developer.jpg"
   },
   {
     name: "Barbara Ramos",
     role: "Graphic Designer",
-    image: "../img/barbara-ramos-graphic-designer.jpg"
+    image: "barbara-ramos-graphic-designer.jpg"
   }
 ]
 
 const output = document.querySelector('.output');
 
-for (let member of team){
-  console.log('name: ' + member.name);
-  console.log('role: ' + member.role);
-  console.log('image tag: ' + member.image);
+// creare const del bottone add e add event listener con click, addNewMember
+
+drawTeam();
+
+function drawTeam(){
+  // resetto il contenuto dell'output
+
+  output.innerHTML = '';
+
+  team.forEach((teamMember) => drawTeamMember(teamMember));
+  
 }
 
-for (let member of team){
-  const card = `
+function drawTeamMember(teamMember){
+
+  let prevContent = output.innerHTML;
+
+  const name = teamMember.name;
+  const role = teamMember.role;
+  const image = teamMember.image;
+
+  const qualities = teamMember.qualities;
+
+
+  prevContent += `
   <div class="card m-4 p-0" style="width: 18rem; border: none;">
-  <img src="${member.image}" class="card-img-top" alt="${member.name}">
+  <div class="card-img-container"><img src="../img/${image}" class="card-img-top" alt="${name}"></div>
   <div class="card-body">
-    <h5 class="card-title text-secondary">${member.name}</h5>
-    <p class="card-text text-secondary">${member.role}</p>
+    <h5 class="card-title text-secondary">${name}</h5>
+    <p class="card-text text-secondary">${role}</p>
+    <p>${qualities.join(' - ')}</p>
   </div>
   </div>
   `;
 
-  output.innerHTML += card;
+  output.innerHTML = prevContent;
+
 }
 
+// creare form html
+
+function addNewMember() {
+  const name = document.getElementById('name').value;
+  const role = document.getElementById('role').value;
+  const image = document.getElementById('image').value;
+  const qualities = document.getElementById('qualities').value;
+
+  const newTeamMember = {
+    name: name,
+    role: role,
+    image: image,
+    qualities: qualities.split(',')
+  }
+
+  drawTeamMember(newTeamMember);
+
+  team.push(newTeamMember);
+
+  resetForm();
+
+}
+
+function resetForm(){
+  document.getElementById('name').value = '';
+  document.getElementById('role').value = '';
+  document.getElementById('image').value = '';
+  document.getElementById('qualities').value = '';
+}
